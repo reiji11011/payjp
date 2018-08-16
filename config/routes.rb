f3046f2b4853c327fa2eaedfe138dca638c2a3df
reiji <i.can.fly1005@ezweb.ne.jp>
2018-08-17 00:50:23 +0900

    [add]課金処理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products do
+    collection do
+      post 'purchase'
+    end
+  end
 end
